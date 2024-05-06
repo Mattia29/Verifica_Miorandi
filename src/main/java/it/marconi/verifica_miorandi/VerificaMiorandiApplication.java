64464ee27af4cc43c6beb89e0fbc61479f53d19c
mattia.miorandi <mattia.miorandi@marconirovereto.it>
2024-05-06 08:32:14 +0200

    Verifica_Miorandi

---
+++ src/main/java/it/marconi/verifica_miorandi/VerificaMiorandiApplication.java
@@ -0,0 +1,13 @@
+package it.marconi.verifica_miorandi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerificaMiorandiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VerificaMiorandiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/marconi/verifica_miorandi/VerificaMiorandiApplicationTests.java
@@ -0,0 +1,13 @@
+package it.marconi.verifica_miorandi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VerificaMiorandiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
