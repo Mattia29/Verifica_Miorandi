98e6655aa986ac758f6a1c8fd85244d098b92f47
mattia.miorandi <mattia.miorandi@marconirovereto.it>
2024-05-06 10:26:07 +0200

    fine verifica

--- src/main/java/it/marconi/verifica_miorandi/controllers/UserController.java
+++ src/main/java/it/marconi/verifica_miorandi/controllers/UserController.java
@@ -1,2 +1,50 @@
 package it.marconi.verifica_miorandi.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import it.marconi.verifica_miorandi.domains.Film;
+import it.marconi.verifica_miorandi.services.UserService;
+
+
+@Controller
+@RequestMapping("/films")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+
+    @GetMapping
+    public String catalogoFilm(Model model) {
+        List<Film> catalogoFilm = userService.getListaFilm();
+        model.addAttribute("catalogoFilm", catalogoFilm);
+        model.addAttribute("nuovoFilm", new Film());
+        return "catalogo";
+    }
+
+    @PostMapping("/films/nuovo")
+    public String aggiungiFilm(@ModelAttribute("nuovoFilm") Film nuovoFilm) {
+        userService.aggiungiFilm(nuovoFilm);
+        return "redirect:/films";
+    }
+
+    @GetMapping("/films/{codice}")
+    public String dettagliProdotto(@PathVariable String codice, Model model) {
+        Film film = userService.getFilmByCodice(codice);
+        model.addAttribute("film", film);
+        return "dettaglioFilm";
+    }
+
+
+}
+
+
--- src/main/java/it/marconi/verifica_miorandi/services/UserService.java
+++ src/main/java/it/marconi/verifica_miorandi/services/UserService.java
@@ -1,2 +1,30 @@
 package it.marconi.verifica_miorandi.services;
 
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.stereotype.Service;
+import it.marconi.verifica_miorandi.domains.Film;
+
+@Service
+public class UserService {
+   
+    private List<Film> listaFilm = new ArrayList<>();
+
+    public List<Film> getListaFilm() {
+        return listaFilm;
+    }
+
+    public void aggiungiFilm(Film film) {
+        listaFilm.add(film);
+    }
+   
+    public void svuotaCatalogo() {
+        listaFilm.clear();
+    }
+   
+    public Film getFilmByCodice(String codice) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getFilmByCodice'");
+    }
+   
+}
